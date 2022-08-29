46a64b64d4ff07f295a21a2cb8a8fad2e43f1b2f
Roni Tammisalo <81182631+rtammisalo@users.noreply.github.com>
2022-08-29 22:00:48 +0300

    Add 11.9 simple end to end -tests

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,7 +1,17 @@
 describe('Pokedex', function() {
-  it('front page can be opened', function() {
+  beforeEach(function() {
     cy.visit('http://localhost:5000')
+  })
+
+  it('front page can be opened', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('pokemon page can be opened', function() {
+    cy.contains('slowpoke').click()
+    cy.get('.pokemon-info').contains('slowpoke')
+    cy.get('.pokemon-info').contains('oblivious')
+    cy.get('.pokemon-info').contains('regenerator')
   })
 })
