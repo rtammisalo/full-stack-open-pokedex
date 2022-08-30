034e2bd5c1faf49a206cf7f417c462d80521796b
Roni Tammisalo <81182631+rtammisalo@users.noreply.github.com>
2022-08-30 23:38:18 +0300

    Add 11.11health check, endpoints+workflow

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
+app.get('/health', (_req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (_req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
